413f0d122d0903e47bda07927bebcc30aa112fcb
Blue89Azul <life.89.blue@gmail.com>
2020-08-17 16:10:34 +0900

    17.08.2020

--- js/main.js
+++ js/main.js
@@ -22,6 +22,41 @@ $(function() {
     $(".shift-position__change-profile").removeClass("open-change-profile");
   });
 
+  //予定入力画面　スイッチャー
+  $(".change-btn").click(transform);
+
+
+
+  function transform() {
+    $(this).toggleClass("active");
+    var flg = $(this).hasClass("active")
+
+    if (flg == true) {
+      // 予定タイトル
+      $(".form__title").attr('placeholder', 'DONE FOR US タイトル');
+      //スイッチャー文言
+      $(".change-btn-label").html("DONE FOR US");
+      // 予定スロット変更
+      $(".form__times").css("display", "none");
+      $(".form__times-doneForUs").css("display", "block");
+      //色変更
+      $(".color-box").css("display", "none");
+      $("input[type=checkbox].like+label>img").css("display", "block");
+    } else {
+      // 予定タイトル
+      $(".form__title").attr('placeholder', '予定タイトル');
+      //スイッチャー文言
+      $(".change-btn-label").html("ADD PLAN");
+      // 予定スロット変更
+      $(".form__times").css("display", "block");
+      $(".form__times-doneForUs").css("display", "none");
+      //色変更
+      $(".color-box").css("display", "block");
+      $("input[type=checkbox].like+label>img").css("display", "none");
+    }
+
+
+  }
 
 
 
