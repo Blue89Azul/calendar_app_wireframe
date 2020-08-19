efbfd4cef31bd5d8f77e267d991fe7aff0ce6e6f
Blue89Azul <life.89.blue@gmail.com>
2020-08-19 09:42:29 +0900

    19.08.2020

--- wireframe02/js/main.js
+++ wireframe02/js/main.js
@@ -5,4 +5,12 @@ $(function(){
   $(".btn__close").on("click", function() {
     $(".profile").removeClass("open-profile");
   });
+
+  $(".btn__coment-list").on("click", function(){
+    $(".coment-list").addClass("open-coment-list");
+  });
+  $(".btn__close-coment-list").on("click", function(){
+    $(".coment-list").removeClass("open-coment-list");
+  });
+
 });
