3fe9dcfd70c90283da60b09f1a633548b8ffeeb4
Blue89Azul <life.89.blue@gmail.com>
2020-08-24 10:27:56 +0900

    24.08.2020

---
+++ wireframe05/js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+  $(".option-btn__main").on("click", function() {
+    $(".option-btn-items > .sub-item").toggle();
+    $(".contents").toggleClass("content-cover");
+  });
+
+
+  $(".option-btn__acount").on("click", function() {
+    $(".profile").addClass("open-profile");
+  });
+  $(".btn__close").on("click", function() {
+    $(".profile").removeClass("open-profile");
+  });
+
+  $(".option-btn__coment-list").on("click", function() {
+    $(".coment-list").addClass("open-coment-list");
+  });
+  $(".btn__close-coment-list").on("click", function() {
+    $(".coment-list").removeClass("open-coment-list");
+  });
+
+  //予定入力画面　スイッチャー
+  $(".change-btn").on("click", function() {
+    $(this).toggleClass("active");
+    var flg = $(this).hasClass("active");
+    if (flg == true) {
+      $("#add-plan").css("display", "none");
+      $("#done-for-us").css("display", "block");
+      $(".change-btn-label").html("こめんとたち");
+      $(".color-and-like-btn").html("いいね！ついか");
+      $("#like").css("display", "block");
+      $("#color").css("display", "none");
+    } else {
+      $("#add-plan").css("display", "block");
+      $("#done-for-us").css("display", "none");
+      $(".change-btn-label").html("よていついか");
+      $(".color-and-like-btn").html("いろへんこう");
+      $("#like").css("display", "none");
+      $("#color").css("display", "block");
+    }
+  });
+
+
+});
