f5d98bfb105365a4f230832eb64841fe51158f0f
Blue89Azul <life.89.blue@gmail.com>
2020-08-25 19:11:58 +0900

    25.08.2020

---
+++ functions/googleCalendarApi/google-calendar-api.php
@@ -0,0 +1,32 @@
+<?php
+$api_key = "AIzaSyAgPogpsvPDZpQZN4T9MzwP48R8vmc5Vjg";
+$calendar_id = urlencode('japanese__ja@holiday.calendar.google.com');
+$start = date('2020-01-01\T00:00:00\Z');
+$end = date('2020-12-31\T00:00:00\Z');
+
+$url = "https://www.googleapis.com/calendar/v3/calendars/". $calendar_id . "/events?";
+$query = [
+  'key' => $api_key,
+  'timeMin' => $start,
+  'timeMax' => $end,
+  'maxResult' => 50,
+  'orderBy' => 'startTime',
+  'singleEvents' =>'true'
+];
+$complete_url = $url . http_build_query($query);
+
+$result =[];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $complete_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$datas = json_decode(curl_exec($ch));
+curl_close($ch);
+
+foreach($datas->items as $data){
+  $result[$data->start->date] = $data->summary;
+}
+
+echo "<pre>";
+var_dump($result);
+echo "</pre>";
