e7514295ae972b7a5381f81de1b2e5020b94b81c
Blue89Azul <life.89.blue@gmail.com>
2020-09-18 15:21:14 +0900

    18.09.2020

--- home/js/main.js
+++ home/js/main.js
@@ -1,5 +1,6 @@
-$(function(){
-  $(".menuBtn").on("click", function(){
-    
+$(function() {
+  $(".menuBtn").on("click", function() {
+    $(".contents").toggleClass("menu-open");
+    $(".global-menu__item").toggleClass("menu-anime");
   });
 });
